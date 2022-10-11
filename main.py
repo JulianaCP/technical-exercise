854006d9d2c60ff92ae935014c9ed0662b461d93
Juliana <jcampos@axiomcloud.ai>
2022-10-10 19:03:12 -0700

    new strategy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
-Data Capture class and stats object.
+Stats class
 
-This class provides functions for calculating statistics of data, including
+This class provides methods for calculating statistics of data, including
 less, between, and greater.
 
 ==================  ==================================================
@@ -22,7 +22,7 @@ Numbers lower than X:
 >> capture.add(6)
 >> stats = capture.build_stats()
 >> stats.less(4)
-[3, 3]
+2
 
 
 Numbers between X and Y:
@@ -35,7 +35,7 @@ Numbers between X and Y:
 >> capture.add(6)
 >> stats = capture.build_stats()
 >> stats.between(3, 6)
-[3, 3, 4, 6]
+4
 
 
 Numbers greater than X:
@@ -48,7 +48,32 @@ Numbers greater than X:
 >> capture.add(6)
 >> stats = capture.build_stats()
 >> stats.between(3, 6)
-[6, 9]
+2
+
+
+Data Capture class
+
+This class provides methods for handling a number
+collection and the  generation of stats from it.
+
+==================  ==================================================
+Function            Description
+==================  ==================================================
+add                 Adds a number to a collection.
+build_stats         Generates a Stats class instance to use later for
+                    calculating statistics of data.
+==================  ==================================================
+
+Add a number:
+
+>> capture = DataCapture()
+>> capture.add(3)
+
+
+Generate a Stats class:
+
+>> capture = DataCapture()
+>> stats = capture.build_stats()
 
 
 Decorators
@@ -58,7 +83,6 @@ A single decorator is defined: check_arguments is a decorator that
 checks that all of the receives arguments for each of the stats
 methods are integers.
 """
-from queue import PriorityQueue
 
 
 def check_arguments(func):
@@ -90,147 +114,111 @@ def check_arguments(func):
     return func_wrapper
 
 
+class Stats:
+    def __init__(self, sorted_numbers, mappings):
+        self._sorted_numbers = sorted_numbers
+        self._mappings = mappings
+        self._counter_numbers = len(sorted_numbers)
+        self._first_number = sorted_numbers[0]
+        self._last_number = sorted_numbers[-1]
+
+    def less(self, number):
+        """Checks if the sent number is lower than X (being X
+        the number to check that will represent an element
+        in the list).
+
+        :param int number: Stats number.
+        """
+        if self._first_number > number:
+            return 0
+
+        if self._last_number < number:
+            return self._counter_numbers
+
+        return self._mappings[number]["min_index"]
+
+    def greater(self, number):
+        """Checks if the sent number is greater than X (being X
+        the number to check that will represent an element
+        in the list).
+
+        :param int number: Stats number.
+        """
+        if self._first_number > number:
+            return self._counter_numbers
+
+        if self._last_number < number:
+            return 0
+
+        return self._counter_numbers - self._mappings[number]["max_index"] - 1
+
+    def between(self, min_number, max_number):
+        """Checks if the sent number is between X and Y (being X
+        the minimum number to check and Y the maximum number to
+        check.
+
+        :param int min_number: Minimum number to check.
+        :param int max_number: Maximum number to check. .
+        """
+        greater_counter = self.greater(max_number)
+        less_counter = self.less(min_number)
+        return self._counter_numbers - greater_counter - less_counter
+
+
 class DataCapture:
     """The DataCapture object accepts numbers and returns an object for querying
     statistics about the inputs. Specifically, the returned object supports
     querying how many numbers in the collection are less than a value, greater
     than a value, or within a range.
     """
-    def __init__(self):
-        # O(1) complexity.
-        self._numbers = []
+    def __init__(self, max_numbers=1000):
+        self._max_numbers = max_numbers
+        self._numbers_existence = [0] * (max_numbers + 1)
 
     @property
-    def numbers(self):
-        """Returns the private attribute numbers, that contains
+    def numbers_existence(self):
+        """Returns the private attribute numbers existence, that contains
         the list with numbers.
         """
-        return self._numbers
+        return self._numbers_existence
 
     def add(self, number):
         """Adds a number to the numbers list (available numbers to
         manipulate and generate the stats).
         """
-        # O(1) complexity.
-        self._numbers.append(number)
+        self._numbers_existence[number] += 1
 
-    @check_arguments
-    def less(self, number, number_to_check=None):
-        """Checks if the sent number is lower than X (being X
-        the number to check that will represent an element
-        in the list). In case no number to check was
-        sent, the method will return false by default.
+    def _trimmed_and_sort_numbers(self):
+        trimmed_sorted_numbers = []
+        for number, counter in enumerate(self._numbers_existence):
+            if counter != 0:
+                trimmed_sorted_numbers.extend([number] * counter)
+        return trimmed_sorted_numbers
 
-        :param int number: Stats number.
-        :param int, None number_to_check: Number to check. Method with
-        check if the 'number' is lower than this number.
+    @staticmethod
+    def generate_mappings(sorted_numbers):
+        """Generates a mappings of the min and max index of each value
+        to later use to counter the number of numbers according to
+        the less, greater and between methods.
+
+        :param list sorted_numbers:  Sorted list of numbers.
         """
-        # O(1) complexity.
-        return number < number_to_check if number_to_check else False
+        mappings = {}
 
-    @check_arguments
-    def between(self, number, min_number=None, max_number=None):
-        """Checks if the sent number is between X and Y (being X
-        the minimum number to check and Y the maximum number to
-        check. Both values represent an interval and this method
-        will check if the sent number is between this interval.
-        In case no min or max number were, the method will
-        return false by default.
+        # Reverts the sorted list of numbers.
+        inverted_numbers = sorted_numbers[::-1]
 
-        :param int number: Stats number.
-        :param int, None min_number: Minimum number to check. Method with
-        check if the 'number' is greater than this number.
-        :param int, None max_number: Maximum number to check. Method with
-        check if the 'number' is lower than this number.
-        """
-        # O(1) complexity.
-        return min_number <= number <= max_number if min_number and max_number else False
+        # create min and max indexes for each number.
+        for number in sorted_numbers:
+            # index first appearance of the number.
+            min_index = sorted_numbers.index(number)
 
-    @check_arguments
-    def greater(self, number, number_to_check=None):
-        """Checks if the sent number is greater than X (being X
-        the number to check that will represent an element
-        in the list). In case no number to check was
-        sent, the method will return false by default.
+            # index of the last appearance of the number.
+            max_index = len(sorted_numbers) - inverted_numbers.index(number) - 1
 
-        :param int number: Stats number.
-        :param int, None number_to_check: Number to check. Method with
-        check if the 'number' is greater than this number.
-        """
-        # O(1) complexity.
-        return number > number_to_check if number_to_check else False
-
-    def _searcher(self, numbers, validator, *args, selections=None):
-        """This is a recursive function to find all the numbers inside a list that
-        passed a validation (validation could be 'numbers lower than X',
-        'numbers between than X and Y' and 'numbers greater than X'). At the
-        end of the execution it will return the list  of all of the numbers
-        that passed the validation.
-
-        :param list numbers: Number of the list.
-        :param function validator: Validation could be 'numbers lower than X',
-        'numbers between than X and Y' and 'numbers greater than X'.
-        :param tuple, int args: Inputs of the validation function.
-        :param list, None selections: Number that passed the restriction.
-        """
-        # O(n) complexity.
-        # We can assume all values will be less than 1,000 so the recursion
-        # will not failed.
-        if not selections:
-            selections = []
-
-        # if there isn't more numbers to process, stop recursion and return
-        # the extracted numbers.
-        if not numbers:
-            return selections
-
-        # add number and keep analyzing
-        if validator(numbers[0], *args):
-            selections.append(numbers[0])
-
-        # keep processing the list of numbers.
-        return self._searcher(numbers[1:], validator, *args, selections=selections)
-
-    def _sort_numbers(self):
-        """Uses the heapq module in Python to create a priority queue.
-        This implementation has O(log n) time for insertion and extraction.
-        The PriorityQueue uses the same heapq implementation internally and
-        thus has the same time complexity (PriorityQueue is the classic OOP style
-        of implementing and using Priority Queues).
-
-        The priority queue is an advanced type of the queue data structure.
-        Instead of dequeuing the oldest element, a priority queue sorts
-        and dequeues elements based on their priorities. Since the
-        queue.PriorityQueue class needs to maintain the order of
-        its elements, a sorting mechanism is required every time a new
-        element is enqueued. Python solves this by using a binary heap
-        to implement the priority queue. The Python priority
-        queue is built on the heapq module, which is basically a
-        binary heap with O(n) complexity.
-        """
-        # O(1) complexity.
-        # we initialise the PriorityQueue class to operate upon a list.
-        sorted_queue_numbers = PriorityQueue()
-        for number in self._numbers:
-            # Insertion	O(log n) complexity.
-            # Puts an number into the queue (It will be adding the numbers
-            # in ordered).
-            sorted_queue_numbers.put(number)
-
-        # O(n) complexity.
-        # generates a ordered list with the sorted queue.
-        sorted_numbers = []
-        while not sorted_queue_numbers.empty():
-            # Deletion O(log n) complexity.
-            # The get command dequeues the highest priority elements from the queue.
-            # here we are going to be adding one by one all the values of
-            # the ordered queue to a list, basically we are moving from a
-            # ordered priorityQueue to a ordered list (will be easier
-            # to use lists to handle the 'less', 'greater' and 'between'
-            # methods with lists instead of a a queue).
-            sorted_numbers.append(sorted_queue_numbers.get())
-
-        return sorted_numbers
+            # min and max index mapped for each number.
+            mappings[number] = {"max_index": max_index, "min_index": min_index}
+        return mappings
 
     def build_stats(self):
         """Sorts the list of numbers and returns an object for querying
@@ -238,22 +226,17 @@ class DataCapture:
         querying how many numbers in the collection are less than a value, greater
         than a value, or within a range.
         """
-        # O(n) complexity
-        # create an ordered list using the numbers list.
+        # Create an ordered list using the numbers list.
         # Note: I would normally use list.sort() but I discarded that option
-        # since it has a O(n log n) complexity. So, in regards of the
-        # requirement I implemented a priorityQueue, more details in the
-        # sort numbers method.
-        sorted_numbers = self._sort_numbers()
+        # since it has a O(n log n) complexity.
+        sorted_numbers = self._trimmed_and_sort_numbers()
 
-        # O(n) complexity
-        # Dynamic Stats class to handle calls on less, greater and between methods.
-        return type("Stats", (object,), {
-            "_sorted_numbers": sorted_numbers,
-            "less": lambda cls, n: self._searcher(cls._sorted_numbers, self.less, n),
-            "greater": lambda cls, n: self._searcher(cls._sorted_numbers, self.greater, n),
-            "between": lambda cls, min_n, max_n: self._searcher(cls._sorted_numbers, self.between, min_n, max_n)
-        })()
+        # Generates a mappings of the min and max index of each number
+        # to later use.
+        mappings = self.generate_mappings(sorted_numbers)
+
+        # Return Stats instance.
+        return Stats(sorted_numbers, mappings)
 
 
 if __name__ == '__main__':
@@ -264,6 +247,6 @@ if __name__ == '__main__':
     capture.add(4)
     capture.add(6)
     stats = capture.build_stats()
-    print(f"'less(4)' = {stats.less(4)}")
-    print(f"'greater(4)' = {stats.greater(4)}")
-    print(f"'between(3, 6)' = {stats.between(3, 6)}")
+    print(stats.less(4))
+    print(stats.greater(4))
+    print(stats.between(3, 6))
